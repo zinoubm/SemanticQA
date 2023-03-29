81b804cfbc7ecd56eb542c0dec6da66919a16ba8
zinoubm <zinne.eddine.2000@gmail.com>
2023-03-29 07:11:06 +0200

    adding data generation for docx

--- app/tests/data.py
+++ app/tests/data.py
@@ -4,13 +4,10 @@ from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import inch
 from reportlab.lib.enums import TA_CENTER
 from reportlab.platypus import Paragraph
+import docx
 
 
 def dummy_pdf(text: str):
-    # debug
-    print("raw text")
-    print(text)
-
     temp_file_path = "/tmp/temp_file"
 
     # Set up the PDF canvas and page size
@@ -39,3 +36,37 @@ def dummy_txt(text: str):
         file.write(text)
 
     return temp_file_path
+
+
+def dummy_docx(text: str):
+    # temp_file_path = "/tmp/temp_file"
+    temp_file_path = "text.docx"
+
+    # create a new Word document
+    document = docx.Document()
+
+    # add a heading to the document
+    document.add_heading("My Document Title", level=0)
+
+    # add a paragraph to the document
+    paragraph = document.add_paragraph(text)
+
+    # add a bold text run to the paragraph
+    bold_text = paragraph.add_run(text)
+    bold_text.bold = True
+
+    # add a table to the document
+    table = document.add_table(rows=3, cols=3)
+    for i in range(3):
+        row_cells = table.rows[i].cells
+        for j in range(3):
+            row_cells[j].text = f"Row {i+1}, Column {j+1}"
+
+    # save the document to disk
+    document.save(temp_file_path)
+
+    return temp_file_path
+
+
+if __name__ == "__main__":
+    dummy_docx("lorem ipsum wow this is working!")
